72220cd0b38c8b46edde9806724a127d1de8cf0f
Dusan Lukic <dusanlukic404@gmail.com>
2022-04-15 19:27:28 +0200

    changed event to work on mobile properly

--- src/script.js
+++ src/script.js
@@ -84,10 +84,8 @@ inputTip.addEventListener("change", function () {
 
 // Add reset functionality if inputs have values
 inputs.forEach((input) => {
-  input.addEventListener("keyup", function (e) {
-    if (e.keyCode >= 48 && e.keyCode <= 57) {
-      resetBtn.classList.add("reset-btn--active");
-    }
+  input.addEventListener("input", function (e) {
+    resetBtn.classList.add("reset-btn--active");
 
     if (resetBtn.classList.contains("reset-btn--active")) {
       resetBtn.addEventListener("click", initCalc);
